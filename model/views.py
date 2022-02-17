5065f4de93f1ade894f0a6f3e12dc23630d6d434
Andrew <androw.emad@yahoo.com>
2022-02-17 22:43:07 +0200

    added python-magic module

--- model/views.py
+++ model/views.py
@@ -1,16 +1,17 @@
-
-
+import magic
 from django.http import HttpRequest
 from django.http import JsonResponse
 
 def upload_file_view(request: HttpRequest):
     if request.method == "POST":
-        
         if (file:=request.FILES.get("file")):
-            with open("media/test1.png", 'wb') as f:
-                lines = file.readlines()
-                for line in lines:
-                    f.write(line)
+            print(magic.from_buffer(file.read(2048), mime=True))
+            # temp_file_name = tempfile.NamedTemporaryFile()
+            # file_name = f"media/{temp_file_name.name}"
+            # with open(file_name, 'wb') as f:
+            #     lines = file.readlines()
+            #     for line in lines:
+            #         f.write(line)
             # file.write("test.png")
             # file.close()
             #check if video or photo
